b0af26051c55c70aff62a8578c8f5579b164c7eb
hinami.higashi <higashi.h27@gmail.com>
2024-04-22 04:40:16 +0900

    ボウリングの課題を提出

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',')
+
+shots = scores.map { |s| s == 'X' ? 10 : s.to_i }
+
+frames = []
+shots.each_with_index do |shot, _i|
+  if frames.size == 10 # 10フレーム目の場合
+    frames.last << shot
+  elsif frames.last&.sum == 10 # ストライクの場合
+    frames << [shot]
+  elsif frames.last&.size == 1 # 2投目だった場合
+    frames.last << shot
+  else
+    frames << [shot]
+  end
+end
+
+point = 0
+count = 0
+frames.each do |frame|
+  count += 1 # 1フレーム後のフレームを指定するための変数
+  point += if frame[0] == 10 # ストライクが出たとき
+             if count <= 8 # 9,10フレーム目以外の場合
+               if frames[count][0] == 10 # 次のフレームもストライクの場合
+                 frame[0] + frames[count][0] + frames[count + 1][0]
+               else # 次のフレームがストライク以外の場合
+                 frame[0] + frames[count][0] + frames[count][1]
+               end
+             elsif count == 9 # 9フレーム目の場合
+               frame[0] + frames[count][0] + frames[count][1]
+             else # 10フレーム目の場合
+               frame.sum
+             end
+           elsif frame.sum == 10 # スペアが出たとき
+             if count <= 9 # 10フレーム目以外の場合
+               frame.sum + frames[count][0]
+             else # 10フレーム目の場合
+               frame.sum
+             end
+           else # ストライクでもスペアでもないとき
+             frame.sum
+           end
+end
+
+puts point
