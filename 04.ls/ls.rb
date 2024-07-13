a3cc09ce6b6f02db152fcb8a6b37d29f021e5c8e
hinami.higashi <higashi.h27@gmail.com>
2024-07-14 00:17:09 +0900

    変数を整理

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,18 +2,20 @@
 
 require 'optparse'
 
-def prepare_file_listing
-  number_of_columns = 3
-  @sorted_files << nil while @sorted_files.size % number_of_columns != 0
+NUMBER_OF_COLUMNS = 3
 
-  count_row = @sorted_files.size / number_of_columns
-  files_slice = @sorted_files.each_slice(count_row).to_a
-  @transposed_files = files_slice.transpose
+def prepare_file_listing(sorted_files)
+  sorted_files << nil while sorted_files.size % NUMBER_OF_COLUMNS != 0
+
+  count_row = sorted_files.size / NUMBER_OF_COLUMNS
+  files_slice = sorted_files.each_slice(count_row).to_a
+  transposed_files = files_slice.transpose
+  transposed_files
 end
 
-def output_files
-  max_filename = @files.map(&:length).max
-  @transposed_files.each do |file|
+def output_files(files, transposed_files)
+  max_filename = files.map(&:length).max
+  transposed_files.each do |file|
     removed_nil_files = file.compact
     aligned_files = removed_nil_files.map { |g| g.ljust(max_filename, ' ') }
     puts aligned_files.join('  ')
@@ -28,7 +30,8 @@ opt.on('-r') { |v| params[:r] = v }
 
 opt.parse!(ARGV)
 
-@files = params[:a] ? Dir.entries('.') : Dir.glob('*')
-@sorted_files = params[:r] ? @files.sort.reverse : @files.sort
-prepare_file_listing
-output_files
+files = params[:a] ? Dir.entries('.') : Dir.glob('*')
+sorted_files = params[:r] ? files.sort.reverse : files.sort
+transposed_files = prepare_file_listing(sorted_files)
+
+output_files(files, transposed_files)
